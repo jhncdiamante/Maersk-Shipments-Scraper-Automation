35f7d6ea15196ff11e6739c2344b02425f2840dc
johncarlodeveloepr <2023100305@bulsu.edu.ph>
2025-05-14 02:01:41 +0800

    reset df for failed processes

--- app.py
+++ app.py
@@ -63,13 +63,14 @@ except Exception as e:
     print(f"An error occurred: Stopping program. Error: {e}")
 finally:
     if maersk.failed_shipments:
-        failed_df = pd.DataFrame(maersk.failed_shipments, columns=["Failed Shipment IDs"])
-        failed_df.to_csv("Failed_Shipment_Extractions.csv", index=False)
-
+        
         print("The program has finished running. Please check the output file for the results.")
         print(f"There are {len(maersk.failed_shipments)} shipments that failed to process.")
         print(f"Run again with the failed shipment IDs found in the 'Failed_Shipment_Extractions.csv' file\n to get the full data. Do not forget to rename the input and output file for the second run.")
-
+    failed_df = pd.DataFrame(maersk.failed_shipments, columns=["Failed Shipment IDs"])
+    failed_df.to_csv("Failed_Shipment_Extractions.csv", index=False)
     df = pd.DataFrame(data_list)
     df.to_csv("OUTPUT.csv", index=False)
+    print("The program has finished running. Please check the output file for the results.")
 
+    maersk.close()
